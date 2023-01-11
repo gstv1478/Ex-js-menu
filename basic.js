946af26b718f86a6a6310a03cb2d93cf10689b65
gstv1478 <gstv1478@gmail.com>
2023-01-11 17:00:38 -0300

    segundo commit

--- basic.js
+++ basic.js
@@ -73,27 +73,6 @@ const menu = [
     },
   ];
   
-  const sectionCenter = document.querySelector(".section-center");
-  
-  window.addEventListener("DOMContentLoaded", function () {
-    let displayMenu = menu.map(function (item) {
-      // console.log(item);
-  
-      return `<article class="menu-item">
-            <img src=${item.img} alt=${item.title} class="photo" />
-            <div class="item-info">
-              <header>
-                <h4>${item.title}</h4>
-                <h4 class="price">$${item.price}</h4>
-              </header>
-              <p class="item-text">
-                ${item.desc}
-              </p>
-            </div>
-          </article>`;
-    });
-    displayMenu = displayMenu.join("");
-    console.log(displayMenu);
-  
-    sectionCenter.innerHTML = displayMenu;
-  });
+
+
+  export default menu;

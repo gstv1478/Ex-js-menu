c7769af213c446a95f53aa9743fd307eedf22fef
gstv1478 <gstv1478@gmail.com>
2023-01-17 01:21:36 -0300

    terceiro commit

--- app.js
+++ app.js
@@ -71,30 +71,27 @@ const menu = [
       img: "./images/item-9.jpeg",
       desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
     },
+    {
+      id: 10,
+      title: "stake meat",
+      category: "dinner",
+      price: 39.99,
+      img: "./images/item-10.jpeg",
+      desc: `everyday carry squi authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    },
   ];
 
 const sectionCenter = document.querySelector(".section-center");
-const filterBtns = document.querySelectorAll(".filter-btn");
+const container = document.querySelector(".btn-container")
+
 window.addEventListener("DOMContentLoaded", function() {
-    displayMenuItems(menu)
+    displayMenuItems(menu);
+    displayMenuButtons();
+
+    
 });
 
-filterBtns.forEach(function(btn) {
-    btn.addEventListener("click", function (e) {
-        const category = e.currentTarget.dataset.id;
-        const menuCategory = menu.filter(function (menuItem) {
-            if (menuItem.category === category) {
-                return menuItem;
-            }
-        });
 
-        if (category === "all") {
-            displayMenuItems(menu);
-        } else {
-            displayMenuItems(menuCategory);
-        }
-    });
-});
 
 function displayMenuItems(menuItems) {
     let displayMenu = menuItems.map(function (item) {
@@ -111,4 +108,39 @@ function displayMenuItems(menuItems) {
     });
     displayMenu = displayMenu.join("");
     sectionCenter.innerHTML = displayMenu;
+}
+
+function displayMenuButtons() {
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {  
+        values.push(item.category);
+        console.log(values);
+      }
+    return values;
+  },
+  ["all"]
+  );
+  const categoryBtns = categories.map(function (category) {
+    return `<button class="filter-btn" type="button" data-id="${category}">${category}</button>`;
+  })
+  .join("");
+  container.innerHTML = categoryBtns;
+  const filterBtns = container.querySelectorAll(".filter-btn");
+  filterBtns.forEach(function(btn) {
+    btn.addEventListener("click", function (e) {
+        const category = e.currentTarget.dataset.id;
+        const menuCategory = menu.filter(function (menuItem) {
+            if (menuItem.category === category) {
+                return menuItem;
+            }
+        });
+
+        if (category === "all") {
+            displayMenuItems(menu);
+        } else {
+            displayMenuItems(menuCategory);
+        }
+    });
+});
 }
